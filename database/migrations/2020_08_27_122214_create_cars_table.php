9ddde8875187a68074017ddd324821642767ecc4
ahmedbenalaya30 <ahmed.benalaya@ensi-uma.tn>
2020-08-27 14:25:28 +0100

    add car Model,controller and Migration

---
+++ database/migrations/2020_08_27_122214_create_cars_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type');
+            $table->string('brand');
+            $table->string('model');
+            $table->date('available_at');
+            $table->string('fuel');
+            $table->integer('year');
+            $table->string('capacity');
+            $table->integer('pricePerDay');
+            $table->string('img');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
