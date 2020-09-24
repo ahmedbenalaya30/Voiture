ff069802e58f13617c21ce9dfdfbcd34bdca1a0f
raafet-dev1 <raafet069@gmail.com>
2020-09-24 04:00:05 +0200

    commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -60,7 +60,7 @@
           <span class="dropdown-item dropdown-header">15 Notifications</span>
           <div class="dropdown-divider"></div>
           <a href="#" class="dropdown-item">
-            <i class="fas fa-envelope mr-2"></i> 4 new messages
+            <i class="fas fa-envelope mr-2"></i> 3 new messages
             <span class="float-right text-muted text-sm">3 mins</span>
           </a>
          
