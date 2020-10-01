48b12bad2d00411a9acc15a7712056d20e80499f
ahmedbenalaya30 <ahmed.benalaya@ensi-uma.tn>
2020-10-01 11:08:07 +0100

    home page and welcome page updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 use App\Booking;
+use App\Car;
 use Carbon\Carbon;
 
 
@@ -30,12 +31,39 @@ class HomeController extends Controller
         $cars= DB::table('cars')->count();
         $bookings=DB::table('bookings')->count();
         $clients=DB::table('users')->where('role','client')->count();
+        $banned=DB::table('users')->where('role','client')->where('contentious',1)->count();
         $paid=Booking::where('is_paid',1)->count();
         if($bookings!=0)
-        $paid=($paid/$bookings)*100;
+        $paid=(number_format($paid/$bookings, 4))*100;
+
+        $unpaid=Booking::where('is_paid',0)->count();
         $cars1= DB::table('cars')->get();
         $today = Carbon::today()->format('Y-m-d');
         $notifications=array();
+        $reservations=DB::table('bookings')->where('is_paid',1)->get();
+        $sommePaye=0;
+        foreach($reservations as $reservation)
+        {
+            $car= Car::find($reservation->car_id);
+            $date1=strtotime($reservation->pick_up_date);
+            $date2=strtotime($reservation->drop_off_date);
+            $nb=$date2-$date1;
+            $nbDay=$nb/86400+1;
+            $price=$nbDay* $car->pricePerDay;
+            $sommePaye= $sommePaye+ $price;
+        }
+        $reservations=DB::table('bookings')->where('is_paid',0)->get();
+        $sommeNonPaye=0;
+        foreach($reservations as $reservation)
+        {
+            $car= Car::find($reservation->car_id);
+            $date1=strtotime($reservation->pick_up_date);
+            $date2=strtotime($reservation->drop_off_date);
+            $nb=$date2-$date1;
+            $nbDay=$nb/86400+1;
+            $price=$nbDay* $car->pricePerDay;
+            $sommeNonPaye= $sommeNonPaye+ $price;
+        }
 
         foreach($cars1 as $car)
         {
@@ -43,7 +71,7 @@ class HomeController extends Controller
             $notifications[]="Assurance expiré".$car->insurance."pour la voiture".$car->carNumber."";
 
         }
-            return view('home',compact('cars','bookings','clients','paid','notifications'));
+            return view('home',compact('cars','bookings','clients','banned','paid','unpaid','notifications','sommePaye','sommeNonPaye'));
     }
         
 }
